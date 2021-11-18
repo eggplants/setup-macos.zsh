df2b311615d8608af5d61235c1f1d0f2c1655551
haruna <w10776e8w@yahoo.co.jp>
2021-11-18 12:50:58 +0900

    Update setup-macos.sh

--- setup-macos.sh
+++ setup-macos.sh
@@ -13,8 +13,9 @@ open /Applications/Docker.app
 brew install \
     act binutils byobu cmake coreutils deno diffutils \
     echo-sd emacs feh findutils gawk gh gnu-sed \
-    gnu-tar gnupg go grep nodenv ncurses nkf mas pinentry-mac \
-    pyenv rbenv shellcheck sl spectacle tcl-tk@8.6.12 \
+    gnu-tar gnupg go grep imagemagick nodenv ncurses \
+    nkf mas pinentry-mac pyenv rbenv shellcheck \
+    sl spectacle tcl-tk@8.6.12 \
     tmux tree uniutils wget wine-crossover w3m yarn
 brew reinstall git nano
 mas install 1429033973 # runcat
